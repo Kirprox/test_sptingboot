ddad21508d2e0eb2b5e5739e67dede3028b95eae
kir <kirpro95@yandex.ru>
2026-04-15 15:23:35 +0300

    add controller methods and response entity

--- src/main/java/ru/testspring/controller/ReservationController.java
+++ src/main/java/ru/testspring/controller/ReservationController.java
@@ -2,15 +2,15 @@ package ru.testspring.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import ru.testspring.model.Reservation;
 import ru.testspring.service.ReservationService;
 
 import java.util.List;
-
 @RestController
+@RequestMapping("/reservation")
 public class ReservationController {
     private static final Logger log = LoggerFactory.getLogger((ReservationController.class));
 
@@ -21,16 +21,23 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @GetMapping("{id}")
-    public Reservation getReservationById(@PathVariable("id") Long id) {
+    public ResponseEntity<Reservation> getReservationById(@PathVariable("id") Long id) {
         log.info("вызвался метод getReservationById: id=" + id);
-        return reservationService.getReservationById(id);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(reservationService.getReservationById(id));
     }
 
     @GetMapping()
-    public List<Reservation> getAllReservations() {
-
+    public ResponseEntity<List<Reservation>> getAllReservations() {
         log.info("вызвался метод getAllReservations");
-        return reservationService.findAllReservation();
-
+        return ResponseEntity.ok(reservationService.findAllReservation());
     }
-}
+
+    @PostMapping()
+    public ResponseEntity<Reservation> createReservation(@RequestBody Reservation reservationToCreate) {
+        log.info("Called createReservation");
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .header("test-header", "123")
+                .body(reservationService.createReservation(reservationToCreate));
+    }
+} //todo "1^47^56"
--- src/main/java/ru/testspring/service/ReservationService.java
+++ src/main/java/ru/testspring/service/ReservationService.java
@@ -4,40 +4,23 @@ import org.springframework.stereotype.Service;
 import ru.testspring.model.Reservation;
 import ru.testspring.model.ReservationStatus;
 
-import java.time.LocalDate;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.concurrent.atomic.AtomicLong;
 
 
 @Service
 public class ReservationService {
 
-    private final Map<Long, Reservation> reservationMap = Map.of(
-            1L, new Reservation(
-                    1L,
-                    100L,
-                    40L,
-                    LocalDate.now(),
-                    LocalDate.now().plusDays(5),
-                    ReservationStatus.APPROVED
-            ),
-            2L, new Reservation(
-                    2L,
-                    110L,
-                    46L,
-                    LocalDate.now(),
-                    LocalDate.now().plusDays(5),
-                    ReservationStatus.APPROVED
-            ),
-            3L, new Reservation(
-                    3L,
-                    100L,
-                    40L,
-                    LocalDate.now(),
-                    LocalDate.now().plusDays(1),
-                    ReservationStatus.APPROVED)
-    );
+    private final Map<Long, Reservation> reservationMap;
+    private final AtomicLong idCounter;
+
+    public ReservationService() {
+        this.reservationMap = new HashMap<>();
+        this.idCounter = new AtomicLong();
+    }
 
 
     public Reservation getReservationById(Long id) {
@@ -49,5 +32,23 @@ public class ReservationService {
 
     public List<Reservation> findAllReservation() {
         return reservationMap.values().stream().toList();
+    }
+
+    public Reservation createReservation(Reservation reservationToCreate) {
+        if (reservationToCreate.id() != null) {
+            throw new IllegalArgumentException("id should be empty");
+        }
+        if (reservationToCreate.status() != null) {
+            throw new IllegalArgumentException("status should be empty");
+        }
+        var newReservation = new Reservation(idCounter.incrementAndGet(),
+                reservationToCreate.userId(),
+                reservationToCreate.roomId(),
+                reservationToCreate.startDate(),
+                reservationToCreate.endDate(),
+                ReservationStatus.PENDING
+        );
+        reservationMap.put(newReservation.id(), newReservation);
+        return newReservation;
     }
 }
